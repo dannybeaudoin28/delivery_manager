e89e580e87165838552b3e929ad5d60d66251b01
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2026-03-21 14:45:41 -0400

    Add first basic deliveries view and confirm proxy works with remote DB

--- myproject/deliverymanager/urls.py
+++ myproject/deliverymanager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all_deliveries', views.all_deliveries, name="deliveries"),
+    path('deliveries/', views.all_deliveries, name="deliveries"),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'deliverymanager.apps.DeliverymanagerConfig',
+    'deliverymanager',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "apps.deliverymanager.wsgi.application"
+WSGI_APPLICATION = "myproject.wsgi.application"
 
 
 # Database
