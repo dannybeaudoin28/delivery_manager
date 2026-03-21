99f382881a8888ec7427b5bd1fd8acf6692d9dac
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2026-03-21 16:35:55 -0400

    fixed formatting issues in code

--- myproject/deliverymanager/models.py
+++ myproject/deliverymanager/models.py
@@ -1,14 +1,4 @@
 from django.db import models
-
-class Delivery(models.Model):
-    address = models.CharField(max_length=200)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    route = models.ForeignKey('Route', on_delete=models.SET_NULL, null=True, related_name='deliveries')
-    
-    def get_created_at(self):
-        return self.created_at
     
 class CustomDelivery(Delivery):
     priority_level = models.IntegerField()
@@ -22,3 +12,17 @@ class Driver(models.Model):
     name = models.CharField(max_length=30)
     route = models.OneToOneField(Route, on_delete=models.SET_NULL, null=True)
     
+class Delivery(models.Model):
+    address = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    route = models.ForeignKey(
+        Route, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        related_name='deliveries'
+    )
+    
+    def get_created_at(self):
+        return self.created_at
