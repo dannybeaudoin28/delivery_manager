ce90686a108c7feab134f28a9f4360f999048bba
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2026-04-04 14:20:40 -0400

    Fix script to start proxy

--- myproject/start.ps1
+++ myproject/start.ps1
@@ -1,4 +1,4 @@
-.\venv\Scripts\Activate
+.\venv\Scripts\Activate.ps1
 pip install -r requirements.txt
 
 Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd "proxy"; .\cloud-sql-proxy.exe --credentials-file="key.json" --port=3306 high-balancer-486900-c3:us-central1:deliveries-instance'
